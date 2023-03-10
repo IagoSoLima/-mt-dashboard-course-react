2df0dfb0b226f173a495b9c86fff98e47dce891c
Iago So Lima <iago.souza069@gmail.com>
2023-03-10 20:20:44 -0300

    chore: implement docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
+import { defineConfig } from 'vite'
+import { APP_HOST_URL, APP_PORT } from './src/app.vars'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: APP_HOST_URL,
+    port: APP_PORT
+  },
   plugins: [react()],
   resolve: {
     alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }]
